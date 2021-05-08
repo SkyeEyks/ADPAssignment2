eca7afcd08ba38bf2c3cae5b1a5c50ff90f6fd6b
SkyeEyks <219095523@mycput.ac.za>
2021-05-08 20:36:57 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/MyMap.java
@@ -0,0 +1,69 @@
+package za.ac.cput;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class MyMap<K, V> implements Map<K, V>{
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+
+
+}
